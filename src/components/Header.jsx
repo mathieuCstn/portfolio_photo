cebd7ae89c16bb18c51f4baff8b45cf048543385
mathieuCstn <mathieuconstantin01@gmail.com>
2023-05-31 10:15:53 +0200

    adding some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import { Link, NavLink } from 'react-router-dom'
 import logoSignature from '../assets/logo-signature.svg'
+import './styles/Header.css'
 
 function Header() {
     return (
-        <header>
-            <Link to='/'>
-                <img src={logoSignature} alt="logo" height={250} width={250}/>
-            </Link>
+        <header id='header'>
+            <div className='logo'>
+                <Link to='/'>
+                    <img src={logoSignature} alt="logo"/>
+                </Link>
+            </div>
             <nav>
                 <NavLink to='/'>Home</NavLink>
                 <NavLink to='/gallery'>Gallery</NavLink>
+            </nav>
+            <nav>
                 <NavLink to='/login'>Se connecter</NavLink>
             </nav>
         </header>
